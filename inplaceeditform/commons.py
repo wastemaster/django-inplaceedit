6ca68fd8f9fa29834144ab7d7731875a01e887bd
Евгений <wastemaster@gmail.com>
2022-12-28 10:48:01 +0100

    Update commons.py

--- inplaceeditform/commons.py
+++ inplaceeditform/commons.py
@@ -43,7 +43,7 @@ def get_dict_from_obj(obj):
         if key.endswith('_id'):
             key2 = key.replace('_id', '')
             try:
-                field, model, direct, m2m = obj._meta.get_field_by_name(key2)
+                field = obj._meta.get_field(key2)
                 if isinstance(field, ForeignKey):
                     obj_dict_result[key2] = obj_dict_result[key]
                     del obj_dict_result[key]
@@ -81,10 +81,10 @@ def import_module(name, package=None):
 def get_adaptor_class(adaptor=None, obj=None, field_name=None):
     if not adaptor:
         try:
-            field = obj._meta.get_field_by_name(field_name)[0]
+            field = obj._meta.get_field(field_name)
         except FieldDoesNotExist:
             if has_transmeta:
-                field = obj._meta.get_field_by_name(transmeta.get_real_fieldname(field_name))[0]
+                field = obj._meta.get_field(transmeta.get_real_fieldname(field_name))
         if isinstance(field, models.URLField):
             adaptor = 'url'
         elif isinstance(field, models.EmailField):
